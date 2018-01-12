6c750625159d10bb5a7c09d09bcbd97830d3f7d7
Shi Ye <s.yephysics@gmail.com>
2018-01-12 14:29:42 -0600

    Add files via upload

---
+++ interactive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#MSUB -N interactive
+#MSUB -M shiye2015@u.northwestern.edu
+#MSUB -e pyerror.out
+#MSUB -o pyoutput.out
+#MSUB -l nodes=1:ppn=2
+####MSUB -l naccesspolicy=singlejob
+#MSUB -l walltime=23:0:0
+#MSUB -A b1011
+#MSUB -q ligo
