8dfccb07755ab22878607c4043c932d31c476ae0
Chaire Ye <claireshiye@tpb67.sunnyvale>
2023-10-14 14:44:22 -0400

    Updates from rickt 101423

--- ns_history.py
+++ ns_history.py
@@ -16,7 +16,7 @@ import ns
 import ns_tidalcapture_hdf5 as ntc_hdf5
 #from scipy import stats
 
-sys.path.insert(1, '/projects/b1095/syr904/MyCodes/cmctoolkit')
+sys.path.insert(1, '/fs/lustre/cita/claireshiye/cmctoolkit')
 import cmctoolkit as cmct
 
 yearsc=31557600.
